d25da85d1fc2cfaec5b052db63ebaf996600e7c6
GANZO9055 <kosta.maslihov@gmail.com>
2025-07-05 22:38:13 +0300

    feat: update database

--- src/main/resources/db/scripts/005_dml_insert_users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users(name, email, age, weight, height, goal, password) VALUES
-('Иван Иванов', 'ivan.ivanov@example.com', 25, 70.5, 175.0, 'WEIGHT LOSS', 'password123'),
-('Мария Петрова', 'maria.petrova@example.com', 30, 60.0, 165.0, 'WEIGHT GAIN', 'securepass456'),
-('Алексей Смирнов', 'alexey.smirnov@example.com', 28, 80.0, 180.0, 'MAINTENANCE', 'mypassword789');
