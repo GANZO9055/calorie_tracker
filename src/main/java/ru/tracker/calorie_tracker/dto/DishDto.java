d3cbbd36596957d158e6dfd882404f5ddde96bfd
GANZO9055 <kosta.maslihov@gmail.com>
2025-07-07 22:10:00 +0300

    feat: add validation User and Dish

--- src/main/java/ru/tracker/calorie_tracker/dto/DishDto.java
+++ src/main/java/ru/tracker/calorie_tracker/dto/DishDto.java
@@ -1,5 +1,6 @@
 package ru.tracker.calorie_tracker.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DishDto {
     private Long id;
+    @NotNull
     private String name;
+    @NotNull
     private Integer calories;
+    @NotNull
     private Integer protein;
+    @NotNull
     private Integer fat;
+    @NotNull
     private Integer carbs;
 }
--- src/main/java/ru/tracker/calorie_tracker/dto/UserDto.java
+++ src/main/java/ru/tracker/calorie_tracker/dto/UserDto.java
@@ -1,7 +1,12 @@
 package ru.tracker.calorie_tracker.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import ru.tracker.calorie_tracker.model.Dish;
 
@@ -12,12 +17,22 @@ import java.util.List;
 @Builder
 public class UserDto {
     private Long id;
+    @NotNull
     private String name;
+    @NotNull
     private String email;
+    @NotNull
+    @Min(1)
+    @Max(100)
     private Integer age;
+    @NotNull
     private Double weight;
+    @NotNull
     private Double height;
+    @NotNull
     private Goal goal;
+    @NotNull
+    @Size(min = 8, max = 20, message = "Password must have least 8 characters and no more then 20")
     private String password;
     private Double calorie;
     private List<Dish> dishes;
