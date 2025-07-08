57845d3e64263d72eb4fcf28aaa56bc966bfc3e0
GANZO9055 <kosta.maslihov@gmail.com>
2025-07-08 22:04:36 +0300

    feat: add handler validation exception

--- src/main/java/ru/tracker/calorie_tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/tracker/calorie_tracker/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package ru.tracker.calorie_tracker.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -21,5 +23,14 @@ public class GlobalExceptionHandler {
                 ),
                 HttpStatus.NOT_FOUND
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handlerValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> messageError = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError ->
+                messageError.put(fieldError.getField(), fieldError.getDefaultMessage())
+        );
+        return new ResponseEntity<>(messageError, HttpStatus.BAD_REQUEST);
     }
 }
